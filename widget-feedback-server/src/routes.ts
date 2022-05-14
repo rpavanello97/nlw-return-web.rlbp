2a6d282a5f11343e2a00d4c7a5e334dfbf9cb5a6
Rafael Bertolim Pavanello <rlbp@gft.com>
2022-05-14 13:44:01 -0300

    new try catch on routes.ts

--- widget-feedback-server/src/routes.ts
+++ widget-feedback-server/src/routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
- 
+
 import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
 import { PrismaFeedbackRepository } from './repositories/prisma/prisma-feedback-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
@@ -10,19 +10,27 @@ export const routes = express.Router();
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
 
-    const prismaFeedbackRepository = new PrismaFeedbackRepository();
-    const mailAdapter = new NodemailerMailAdapter();
+    try {
+        const prismaFeedbackRepository = new PrismaFeedbackRepository();
+        const mailAdapter = new NodemailerMailAdapter();
 
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-        prismaFeedbackRepository,
-        mailAdapter,
-    );
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbackRepository,
+            mailAdapter,
+        );
 
-    await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot,
-    })  
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot,
+        })
 
-    return res.status(201).send();
+        return res.status(201).send();
+
+    } catch (err) {
+        console.log(err);
+
+        return res.status(500).send();
+    }
+
 })
